a9ce8d3407ed9c8e43933e9fdbf5b9a764990014
Grayson Wright <wright.grayson@gmail.com>
2016-01-14 23:43:31 -0800

    Generate configs for download
    Feature:
    
    When I click on the "Download" button,
    I want to download a ZIP file with config files for all of my tools
    so I can easily use my new color scheme.
    
    Implementation:
    
    - Use the submitted colors to create a yaml config file in the format
      that `base16-builder expects
    - Run `base16-builder` to generate config files
    - Zip up the resulting directory into a single file
    - Send the zipped file as a download to the user
    - Remove the intermediate files

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -1,6 +1,56 @@
+require "zip"
+require "zip_file_generator"
+
 class ConfigsController < ApplicationController
   def index
-    # TODO: set up https://github.com/chriskempson/base16-builder
-    render text: params[:colors].split(",")
+    colors = yaml_for_colors(params[:colors].split(","))
+
+    file = Tempfile.new('scheme')
+    file.write(colors)
+
+    output_dir = generate_configs(file.path)
+    zipped = zip_directory(output_dir)
+    send_file zipped
+
+    File.delete(zipped)
+    file.close
+    file.unlink
+  end
+
+  private
+
+  def yaml_for_colors(colors)
+    {
+      "scheme" => "Colors",
+      "author" => "Grayson Wright",
+      "base00" => colors[0],
+      "base01" => colors[1],
+      "base02" => colors[2],
+      "base03" => colors[3],
+      "base04" => colors[4],
+      "base05" => colors[5],
+      "base06" => colors[6],
+      "base07" => colors[7],
+      "base08" => colors[8],
+      "base09" => colors[9],
+      "base0A" => colors[10],
+      "base0B" => colors[11],
+      "base0C" => colors[12],
+      "base0D" => colors[13],
+      "base0E" => colors[14],
+      "base0F" => colors[15],
+    }.to_yaml
+  end
+
+  def generate_configs(path)
+    `./vendor/base16-builder/base16 -s #{path}`
+    "vendor/base16-builder/output"
+  end
+
+  def zip_directory(path)
+    zipfile_path = "tmp/colors.zip"
+
+    ZipFileGenerator.new(path, zipfile_path).write
+    zipfile_path
   end
 end
