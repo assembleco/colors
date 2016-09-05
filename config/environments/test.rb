e8f4ca5dcaf87c217303d149a419fa7220c0cc36
Grayson Wright <wright.grayson@gmail.com>
2016-09-04 17:41:02 -0700

    Update config setting for Rails 5

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
   config.cache_classes = true
   config.eager_load = false
-  config.serve_static_files   = true
+  config.public_file_server.enabled = true
   config.static_cache_control = 'public, max-age=3600'
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
